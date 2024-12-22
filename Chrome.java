5a3ac087ff4896986b287e1e9e6c7a8130358098
naravindkumar1992 <n.aravindkumar1992@gmail.com>
2024-12-22 14:49:37 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		
+		float a=91.0f;
+		
+		String developer="google";
+		
+		boolean isBeta=false;
+		
+		int releaseYear=2008;
+		
+		char shortCutKey='C';
+		
+		System.out.println(a);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortCutKey);
+		
+	}
+
+}
